3fe1ae71dc7b9adcbe8b552c1c61a684b59847c5
cpazstido <120964417@qq.com>
2018-12-10 13:16:53 +0800

    zk

--- src/test/com/cf/sessiontest/test/ZookeeperTest.java
+++ src/test/com/cf/sessiontest/test/ZookeeperTest.java
@@ -9,21 +9,127 @@ import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
 
 public class ZookeeperTest {
+    private static String address = "10.10.1.231:2181";
+
+    /**
+     * 创建永久节点
+     */
+    @Test
+    public void createPersistentNode(){
+        CuratorFramework client = CuratorFrameworkFactory.newClient(address, new ExponentialBackoffRetry(1000, 3));
+
+        try {
+            client.start();
+            String path = "/cf/PERSISTENT/b";
+
+            client.create()
+                    .creatingParentContainersIfNeeded()
+                    .withMode(CreateMode.PERSISTENT)
+                    .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                    .forPath(path, "hello, zk".getBytes());
+            System.out.println("b");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(client!=null)
+                client.close();
+        }
+    }
+
+    /**
+     * 创建永久有序节点
+     */
+    @Test
+    public void createPersistentSequentialNode(){
+        CuratorFramework client = CuratorFrameworkFactory.newClient(address, new ExponentialBackoffRetry(1000, 3));
+
+        try {
+            client.start();
+            String path = "/cf/PERSISTENT_SEQUENTIAL/b";
+
+            client.create()
+                    .creatingParentContainersIfNeeded()
+                    .withMode(CreateMode.PERSISTENT_SEQUENTIAL)
+                    .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                    .forPath(path, "hello, zk".getBytes());
+            System.out.println("b");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(client!=null)
+                client.close();
+        }
+    }
+
+    /**
+     * 创建临时节点
+     */
+    @Test
+    public void createEphemeralNode(){
+        CuratorFramework client = CuratorFrameworkFactory.newClient(address, new ExponentialBackoffRetry(1000, 3));
+
+        try {
+            client.start();
+            String path = "/cf/EPHEMERAL/b";
+
+            client.create()
+                    .creatingParentContainersIfNeeded()
+                    .withMode(CreateMode.EPHEMERAL)
+                    .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                    .forPath(path, "hello, zk".getBytes());
+            System.out.println("b");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(client!=null)
+                client.close();
+        }
+    }
+
+    /**
+     * 创建临时有序节点
+     */
+    @Test
+    public void createEphemeralSequentialNode(){
+        CuratorFramework client = CuratorFrameworkFactory.newClient(address, new ExponentialBackoffRetry(1000, 3));
+
+        try {
+            client.start();
+            String path = "/cf/EPHEMERAL_SEQUENTIAL/b";
+
+            for(int i=0;i<10;i++){
+                client.create()
+                        .creatingParentContainersIfNeeded()
+                        .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+                        .forPath(path, "hello, zk".getBytes());
+            }
+            System.out.println("b");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(client!=null)
+                client.close();
+        }
+    }
 
     @Test
     public void test() throws Exception{
-        String address = "10.10.1.231:2181";
         CuratorFramework client = CuratorFrameworkFactory.newClient(address, new ExponentialBackoffRetry(1000, 3));
 
         try{
             client.start();
 
-            String path = "/cf/persistent_sequential/a";
+            String path = "/cf/persistent_sequential/b";
 
             //a. 创建永久性节点
             client.create()
                     .creatingParentContainersIfNeeded()
-                    .withMode(CreateMode.PERSISTENT_SEQUENTIAL)
+                    .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
                     .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
                     .forPath(path, "hello, zk".getBytes());
             System.out.println("b");
